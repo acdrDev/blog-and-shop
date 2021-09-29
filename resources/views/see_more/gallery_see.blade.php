95e75c91ded31fe8f018ff5b7274cd15ef5dee14
Petelgeuse Romanee-Conti <petelgeuseromaneeconti@gmail.com>
2021-09-29 00:58:29 -0500

    small fixes

--- resources/views/see_more/gallery_see.blade.php
+++ resources/views/see_more/gallery_see.blade.php
@@ -9,7 +9,7 @@
                 <img src="{{$product->url_path}}">
                 <div>
                 <h2>{{$product->title}}</h2>
-                <p class="text">{{$product->description}}</p>
+                <p class="text">{!!$product->description!!}</p>
                 <div class="section_publications"> 
                   <a href="{{url()->previous()}}"><button>Volver</button></a>
                   <button>Descargar</button>
--- resources/views/see_more/shop_see.blade.php
+++ resources/views/see_more/shop_see.blade.php
@@ -9,7 +9,8 @@
                 <img src="{{$product->url_path}}">
                 <div>
                 <h2>{{$product->title}}</h2>
-                <p class="text">{{$product->description}}</p>
+                <p class="text">{!!$product->description!!}</p>
+                <div class="products"><h3>${{$product->price}}</h3></div>
                 <div class="section_publications"> 
                   <a href="{{url()->previous()}}"><button>Volver</button></a>
                   <button>Comprar</button>
